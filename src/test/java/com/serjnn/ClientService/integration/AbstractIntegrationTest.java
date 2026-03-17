ca37e3d35f0aa02859f43f2b029103283c6d4209
sergey <sersh200606@gmail.com>
2026-03-17 16:27:19 +0300

    fix: flyway security

--- src/test/java/com/serjnn/ClientService/integration/AbstractIntegrationTest.java
+++ src/test/java/com/serjnn/ClientService/integration/AbstractIntegrationTest.java
@@ -1,10 +1,13 @@
 package com.serjnn.ClientService.integration;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 @Testcontainers
 public abstract class AbstractIntegrationTest {
 
--- src/test/java/com/serjnn/ClientService/integration/ClientIntegrationTest.java
+++ src/test/java/com/serjnn/ClientService/integration/ClientIntegrationTest.java
@@ -23,8 +23,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+
 public class ClientIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
